723839c447e95f1dbc3c0ef4c5eb79d748f2c43c
serafin625 <serafin.m625@gmail.com>
2022-03-22 10:27:59 +0000

    menu

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner odczyt = new Scanner(System.in);
+    int wybor;
+    System.out.println("wybierz (1-4)");
+    System.out.println("1. Suma pól 2 kół");
+    System.out.println("2. Pole prostokąta");
+    System.out.println("3. Pole trójkąta prostokątnego");
+    System.out.println("4. Suma pół 2 kwadratów");
+    wybor=odczyt.nextInt();
+    
   }
 }
