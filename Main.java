09e25d3a8786b18bb59e8f3cd4da937d68f32b81
serafin625 <serafin.m625@gmail.com>
2022-04-03 11:15:46 +0000

    zabezpieczenie przed wartościami <=0

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main {
   public static void main(String[] args) {
     Scanner odczyt = new Scanner(System.in);
     int wybor;
-    double r1, r2, pole, a, b;
+    double r1, r2, pole, pole2, a, b;
     double pi=3.14;
     System.out.println("wybierz (1-4)");
     System.out.println("1. Suma pól 2 kół");
@@ -17,6 +17,10 @@ class Main {
         r1=odczyt.nextDouble();
         System.out.println("Podaj promien 2. koła");
         r2=odczyt.nextDouble();
+        if (r1<=0 || r2<=0){
+          System.out.println("Złe dane;");
+          break;
+        }
         r1=Math.pow(r1,2);
         r2=Math.pow(r2,2);
         pole=pi*r1+pi*r2;
@@ -27,6 +31,10 @@ class Main {
         a=odczyt.nextDouble();
         System.out.println("Podaj długośc boku b: ");
         b=odczyt.nextDouble();
+        if (a<=0 || b<=0){
+          System.out.println("Złe dane;");
+          break;
+          }
         pole=a*b;
         System.out.println("Pole prostokąta to: "+pole);
       break;
@@ -34,8 +42,30 @@ class Main {
         System.out.println("Podaj bok a oraz b: ");
         a=odczyt.nextDouble();
         b=odczyt.nextDouble();
-        
+        if (a<=0 || b<=0){
+          System.out.println("Złe dane;");
+          break;
+          }
+        pole=(a*b)/2;
+        System.out.println("Pole trójkąta prostokątnego wynosi: "+pole);
       break;
+      case 4:
+        System.out.println("Podaj długośc boku kwadratu nr 1: ");
+        a=odczyt.nextDouble();
+        System.out.println("Podaj długośc boku kwadratu nr 1: ");
+        b=odczyt.nextDouble();
+        if (a<=0 || b<=0){
+          System.out.println("Złe dane;");
+          break;
+          }
+        pole=Math.pow(a,2);
+        pole2=Math.pow(b,2);
+        double wynik = pole+pole2;
+        System.out.println("Suma pól wynosi"+wynik);
+      break;
+      default: 
+      break;
+      
     }
   }
 }
